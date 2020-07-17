5fd9475af5c7b5c9fe9f1bcc1d401af22bfc300a
kamkali <kamkalis@gmail.com>
2020-07-17 11:53:39 +0200

    [BD-68] Avro file to python loader + conversion to pandas DataFrame

---
+++ PyLoader/loader.py
@@ -0,0 +1,23 @@
+from avro.datafile import DataFileReader
+from avro.io import DatumReader
+from pathlib import Path
+import pandavro as pdx
+import json
+
+
+class AvroLoader:
+    def __init__(self, schema_config_path=Path(__file__).parent.parent.absolute().joinpath('config/avro_schema.json')):
+        self.schema_config_path = schema_config_path
+
+    def __load_schema(self):
+        with open(self.schema_config_path) as f:
+            json_schema = json.load(f)
+        return json_schema
+
+    def read_avro_to_pandas_df(self, path_to_file, sort=True, sort_by='sequenceId'):
+        df = pdx.read_avro(path_to_file, self.__load_schema())
+        return df.sort_values(sort_by) if sort else df
+
+    @staticmethod
+    def get_avro_reader(avro_file_path):
+        return DataFileReader(open(avro_file_path, "rb"), DatumReader())
