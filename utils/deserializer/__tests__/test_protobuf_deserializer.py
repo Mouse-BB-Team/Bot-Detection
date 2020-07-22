ebd8f1853d393afe260b4176d29ad1b611231f71
kamkali <kamkalis@gmail.com>
2020-07-22 16:16:59 +0200

    [BD-83] Review changes

--- utils/deserializer/__tests__/test_protobuf_deserializer.py
+++ utils/deserializer/__tests__/test_protobuf_deserializer.py
@@ -1,3 +1,5 @@
+import sys
+
 from utils.deserializer.protobuf_deserializer import ProtoLoader
 from pathlib import Path
 import pandas as pd
@@ -6,19 +8,17 @@ import pytest
 PROTOFILES_DIR_PATH = Path(__file__).parent.joinpath("protofilesdir").absolute().__str__()
 
 
-def test_should_return_single_df_sequence():
+@pytest.mark.parametrize('filepath', ["test_file.pb", "test_file_1.txt", "test_file_2.xml"])
+def test_should_return_single_df_sequence(filepath):
     loader = ProtoLoader(PROTOFILES_DIR_PATH)
-    filepath = Path(PROTOFILES_DIR_PATH).joinpath("test_file.pb").__str__()
     sequence = loader.get_single_sequence(filepath)
     assert isinstance(sequence, pd.DataFrame)
 
 
-@pytest.mark.parametrize('arg', ["", ".txt", ".json", ".csv", ".xml"])
-def test_should_raise_value_error_when_wrong_file_ext(arg):
+def test_should_return_not_none_when_directory_not_empty():
     loader = ProtoLoader(PROTOFILES_DIR_PATH)
-    filepath = Path(PROTOFILES_DIR_PATH).joinpath(f"test_file{arg}").__str__()
-    with pytest.raises(ValueError):
-        loader.get_single_sequence(filepath)
+    seq_list = loader.get_list_of_sequences()
+    assert seq_list is not None
 
 
 def test_should_return_correct_length_of_seq_list():
@@ -27,14 +27,20 @@ def test_should_return_correct_length_of_seq_list():
     assert len(seq_list) == 3
 
 
+def test_should_return_empty_list_when_directory_empty():
+    loader = ProtoLoader(PROTOFILES_DIR_PATH + "/wrong/path")
+    seq_list = loader.get_list_of_sequences()
+    assert len(seq_list) == 0
+
+
+def test_should_check_for_list_when_directory_empty():
+    loader = ProtoLoader(PROTOFILES_DIR_PATH + "/wrong/path")
+    seq_list = loader.get_list_of_sequences()
+    assert isinstance(seq_list, list)
+
+
 def test_should_return_list_of_sequences():
     loader = ProtoLoader(PROTOFILES_DIR_PATH)
     seq_list = loader.get_list_of_sequences()
     for seq in seq_list:
         assert isinstance(seq, pd.DataFrame)
-
-
-def test_should_raise_file_not_found_error():
-    loader = ProtoLoader(PROTOFILES_DIR_PATH + "/wrong/path")
-    with pytest.raises(FileNotFoundError):
-        loader.get_list_of_sequences()
