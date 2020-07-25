834e685180c2f6284c494c7d4f5d632b23598f1f
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-25 17:38:59 +0200

    new solution

---
+++ utils/git-observer/run-commit-execution.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh $PLG_USERNAME@pro.cyfronet.pl 'cd Bot-Detection && \
+git checkout $OBSERVED_BRANCH && \
+git pull origin $OBSERVED_BRANCH && \
+./utils/git-observer/./example.sh'
