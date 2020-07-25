1b9d805d1dbc40fe22b77d8c6ebbf2762fd49699
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-25 18:13:26 +0200

    added support for zsh

--- utils/git-observer/run-plgrid-job.sh
+++ utils/git-observer/run-plgrid-job.sh
@@ -2,4 +2,4 @@
 
 lastCommitHash=$(git rev-parse --short HEAD)
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting job for: '$lastCommitHash'"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting job for: '\#$lastCommitHash'"}' https://hooks.slack.com/services/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
