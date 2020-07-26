cd660d4a3ce4ba64f0077440e5d04d1f63bcf537
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-26 22:28:15 +0200

    fixes

--- utils/git-observer/run-commit-execution.sh
+++ utils/git-observer/run-commit-execution.sh
@@ -3,4 +3,5 @@
 ssh "$PLG_USERNAME"@pro.cyfronet.pl 'cd ~/Bot-Detection && \
 git checkout $OBSERVED_BRANCH && \
 git pull origin $OBSERVED_BRANCH && \
-./utils/git-observer/./run-plgrid-job.sh'
+cd utils/git-observer && \
+./run-plgrid-job.sh'
