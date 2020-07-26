bbe2e1fd660d8759056fdd3ea6ac1b527bcf9896
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-07-26 22:57:03 +0200

    git alias for pushing and running plgrid job in one commend

---
+++ utils/git-observer/create-git-alias.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+create_alias()
+{
+  echo '[alias]
+        deploy = "!f() { git push \"$1\" \"$2\"; utils/git-observer/./run-commit-execution.sh; }; f"' >> .git/config
+}
+
+while true; do
+    read -p "Do you want to make git alias \"git deploy <remote> <branch>\" to pushing and running plgrid job in one commend? (y/n): " yn
+    case $yn in
+        [Yy]* ) create_alias; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
