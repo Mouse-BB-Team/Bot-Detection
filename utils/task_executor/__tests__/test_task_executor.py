6f4bb53bd845bff453c931599a9e374be31eeaea
kamkali <kamkalis@gmail.com>
2020-08-02 19:45:54 +0200

    [BD-88] Quick Fix

--- utils/task_executor/__tests__/test_task_executor.py
+++ utils/task_executor/__tests__/test_task_executor.py
@@ -1,6 +1,6 @@
 import pytest
 from utils.task_executor.task_executor import TaskExecutor
-from utils.task_executor.__tests__.mocks.ml_model_mock import MlModelMock, ExceptionModelMock
+from utils.task_executor.__tests__.mocks.ml_model_mock import MlModelMock
 
 
 @pytest.mark.parametrize('arg', [0, -1])
@@ -14,7 +14,7 @@ def test_should_raise_value_error_when_illegal_argument(arg):
 def test_should_execute_and_return_list():
     model = MlModelMock()
     executor = TaskExecutor(model)
-    result = executor.start_execution()
+    result = executor.start_execution(number_of_task_to_run=10)
     assert isinstance(result, list)
 
 
