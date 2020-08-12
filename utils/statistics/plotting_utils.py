a7d139a8c82c60f381c3646dedf92ec7cd096649
kamkali <kamkalis@gmail.com>
2020-08-12 12:07:52 +0200

    Commit

--- utils/statistics/plotting_utils.py
+++ utils/statistics/plotting_utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import subprocess
 import os
+import csv
 from pathlib import Path
 from utils.imgur_uploader.imgur_uploader import ImgurUploader
 
@@ -8,37 +9,47 @@ from utils.imgur_uploader.imgur_uploader import ImgurUploader
 class PlottingUtils:
     def __init__(self):
         self.uploaded_plots = dict()
+        self.__plotted_data = dict()
         self.__uploader = ImgurUploader()
         self.__commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
+        self.__output_dir_path = Path(__file__).parent.joinpath("outputs")
+        self.__commit_dir_path = Path(self.__output_dir_path).joinpath(self.__commit_hash)
 
-    def create_plot(self, metric, metric_arr, val_metric_arr):
+    def create_plot(self, metric_name, metric_arr, val_metric_arr):
         plt.plot(metric_arr)
         plt.plot(val_metric_arr)
-        plt.title(f'model {metric}')
-        plt.ylabel(metric)
+        plt.title(f'model {metric_name}')
+        plt.ylabel(metric_name)
         plt.xlabel('epoch')
         plt.legend(['train', 'test'], loc='upper left')
 
-        image_path = self.save_to_file(metric)
+        image_path = self.__save_image(metric_name)
         uploaded_url = self.__uploader.upload_image(image_path)
-        self.uploaded_plots[metric] = uploaded_url
+        self.uploaded_plots[metric_name] = uploaded_url
+        self.__plotted_data[metric_name] = metric_arr
+        self.__plotted_data[f'val_{metric_name}'] = val_metric_arr
 
         plt.clf()
         plt.cla()
+# TODO to csv
+# TODO percentile
+    def __save_image(self, metric_name):
+        if not os.path.exists(self.__output_dir_path):
+            os.mkdir(self.__output_dir_path)
+        if not os.path.exists(self.__commit_dir_path):
+            os.mkdir(self.__commit_dir_path)
 
-    def save_to_file(self, metric):
-        output_path = Path(__file__).parent.joinpath("outputs")
-        if not os.path.exists(output_path):
-            os.mkdir(output_path)
-        commit_dir_path = Path(output_path).joinpath(self.__commit_hash)
-        if not os.path.exists(commit_dir_path):
-            os.mkdir(commit_dir_path)
-
-        image_path = Path(__file__).parent \
-            .joinpath("outputs") \
-            .joinpath(f'{self.__commit_hash}') \
-            .joinpath(f"{self.__commit_hash}_{metric}_plot.png")
+        image_path = self.__commit_dir_path.joinpath(f"{self.__commit_hash}_{metric_name}_plot.png")
 
         plt.savefig(image_path)
 
         return image_path
+
+    def save_plotted_data_to_csv(self):
+        csv_path = self.__commit_dir_path.joinpath(f"{self.__commit_hash}_data.csv")
+        with open(csv_path, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=self.__plotted_data.keys())
+            writer.writeheader()
+            rows = zip(*[data for data in self.__plotted_data])
+            for row in rows:
+                writer.writerow(row)
