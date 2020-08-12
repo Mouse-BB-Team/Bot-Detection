8edde968b0f9080d38ed47282c8797b96ac19db5
Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>
2020-08-12 18:00:16 +0200

    [BD-96] Slack chart notification (#15)
    * notifier renamed
    
    * adjust message schema
    
    * removed notification from terminator
    
    * removed unused import

--- utils/slack_notifier/__init__.py
+++ utils/slack_notifier/__init__.py
@@ -1,2 +1,4 @@
-from .slack_notificator import SlackNotifier
-from .slack_message import Message
+from .slack_notifier import SlackNotifier
+from .simple_slack_message import SimpleMessage
+from .result_slack_message import ResultMessage
+from .message_color import Color
