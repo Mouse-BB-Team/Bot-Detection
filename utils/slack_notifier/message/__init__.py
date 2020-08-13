1ea2abba937d70cbf1a846c9f9f8f92c68ac2c43
Kalis <50112357+kamkali@users.noreply.github.com>
2020-08-13 17:08:44 +0200

    Slack Notifier rebuild (#19)
    * Slack Notifier rebuild
    
    * creating http post method

---
+++ utils/slack_notifier/message/__init__.py
@@ -0,0 +1,2 @@
+from .result_slack_message import ResultMessage
+from .simple_slack_message import SimpleMessage
--- utils/slack_notifier/slack_notifier.py
+++ utils/slack_notifier/slack_notifier.py
@@ -1,35 +1,41 @@
 import logging.config
 from pathlib import Path
-import requests
 import json
+from http.client import HTTPSConnection
 
-logger_config_path = Path(__file__).parent.joinpath('logger.config').absolute()
+logger_config_path = Path(__file__).parent.parent.parent.joinpath('config').joinpath('logger.config').absolute()
 logging.config.fileConfig(logger_config_path)
 
 
 class SlackNotifier:
+    __config_path = Path(__file__).parent.parent.parent.joinpath('config').joinpath('slack-config.json')
 
-    def __init__(self, config='../../config/slack-config.json'):
+    def __init__(self, config=__config_path):
         self.__logger = logging.getLogger(self.__class__.__name__)
         self.__config_file = config
         self.__hook_URL = None
+        self.__hook_context = None
         self.__load_config()
 
     def __load_config(self):
         with open(self.__config_file, 'r') as f:
-            self.__hook_URL = json.load(f)['hookURL']
+            loaded_config = json.load(f)
+            self.__hook_URL = loaded_config['hookURL']
+            self.__hook_context = loaded_config['hookContext']
 
     def notify(self, json_message):
+        message = json.dumps(json_message)
+        self.__http_post_request(message)
+
+    def __http_post_request(self, json_msg):
         try:
-            message = json.dumps(json_message)
-            response = requests.post(self.__hook_URL, message)
-            response.raise_for_status()
-            self.__logger.info("message sent")
-        except requests.exceptions.HTTPError as err:
-            self.__logger.error("error when trying to send message", err)
-        except requests.exceptions.ConnectionError as err:
-            self.__logger.error("connection error occurred", err)
-        except requests.exceptions.Timeout as err:
-            self.__logger.error("timeout", err)
-        except requests.exceptions.RequestException as err:
-            self.__logger.error("request error", err)
+            headers = {"Content-type": "application/json"}
+            connection = HTTPSConnection(self.__hook_URL)
+            connection.request("POST", self.__hook_context, json_msg, headers)
+            response = connection.getresponse()
+            if response.status != 200:
+                raise AttributeError(f"Response body: {response.read()}")
+        except Exception as e:
+            raise e
+        finally:
+            connection.close()
