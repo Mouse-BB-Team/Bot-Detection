992ddd87ffbc07e7821026f5d60d1527100d4f19
kamkali <kamkalis@gmail.com>
2020-08-13 16:31:29 +0200

    Pending job notification script

---
+++ utils/notification_jobs/pending_job.py
@@ -0,0 +1,21 @@
+from utils.slack_notifier import *
+from datetime import datetime
+from sys import argv
+
+
+class PendingJobNotification:
+    @staticmethod
+    def notify():
+        start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        notifier = SlackNotifier()
+        simple_msg = SimpleMessage.new_builder() \
+            .with_reporter(argv[1]) \
+            .with_commit_hash(f"#{argv[2]}") \
+            .with_job_time(start_time) \
+            .with_header("PENDING JOB") \
+            .build()
+        notifier.notify(simple_msg)
+
+
+if __name__ == '__main__':
+    PendingJobNotification.notify()
