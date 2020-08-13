39025f5ec74aea393420eaec1ec4e9ebcb69d247
kamkali <kamkalis@gmail.com>
2020-08-13 13:02:02 +0200

    http native requests

--- utils/slack_notifier/slack_notifier.py
+++ utils/slack_notifier/slack_notifier.py
@@ -1,7 +1,8 @@
 import logging.config
 from pathlib import Path
-import requests
 import json
+from http.client import HTTPSConnection
+
 
 logger_config_path = Path(__file__).parent.parent.parent.joinpath('config').joinpath('logger.config').absolute()
 logging.config.fileConfig(logger_config_path)
@@ -14,23 +15,18 @@ class SlackNotifier:
         self.__logger = logging.getLogger(self.__class__.__name__)
         self.__config_file = config
         self.__hook_URL = None
+        self.__hook_context = None
         self.__load_config()
 
     def __load_config(self):
         with open(self.__config_file, 'r') as f:
-            self.__hook_URL = json.load(f)['hookURL']
+            loaded_config = json.load(f)
+            self.__hook_URL = loaded_config['hookURL']
+            self.__hook_context = loaded_config['hookContext']
 
     def notify(self, json_message):
-        try:
-            message = json.dumps(json_message)
-            response = requests.post(self.__hook_URL, message)
-            response.raise_for_status()
-            self.__logger.info("message sent")
-        except requests.exceptions.HTTPError as err:
-            self.__logger.error("error when trying to send message", err)
-        except requests.exceptions.ConnectionError as err:
-            self.__logger.error("connection error occurred", err)
-        except requests.exceptions.Timeout as err:
-            self.__logger.error("timeout", err)
-        except requests.exceptions.RequestException as err:
-            self.__logger.error("request error", err)
+        message = json.dumps(json_message)
+        headers = {"Content-type": "application/json"}
+        connection = HTTPSConnection(self.__hook_URL)
+        connection.request("POST", self.__hook_context, message, headers)
+        connection.close()
