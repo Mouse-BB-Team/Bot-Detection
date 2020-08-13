e63d2358a40e8df89457c5d73af35eea90aff14e
kamkali <kamkalis@gmail.com>
2020-08-13 17:07:16 +0200

    creating http post method

--- utils/slack_notifier/slack_notifier.py
+++ utils/slack_notifier/slack_notifier.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import json
 from http.client import HTTPSConnection
 
-
 logger_config_path = Path(__file__).parent.parent.parent.joinpath('config').joinpath('logger.config').absolute()
 logging.config.fileConfig(logger_config_path)
+
 
 class SlackNotifier:
     __config_path = Path(__file__).parent.parent.parent.joinpath('config').joinpath('slack-config.json')
@@ -25,10 +25,13 @@ class SlackNotifier:
 
     def notify(self, json_message):
         message = json.dumps(json_message)
-        headers = {"Content-type": "application/json"}
+        self.__http_post_request(message)
+
+    def __http_post_request(self, json_msg):
         try:
+            headers = {"Content-type": "application/json"}
             connection = HTTPSConnection(self.__hook_URL)
-            connection.request("POST", self.__hook_context, message, headers)
+            connection.request("POST", self.__hook_context, json_msg, headers)
             response = connection.getresponse()
             if response.status != 200:
                 raise AttributeError(f"Response body: {response.read()}")
