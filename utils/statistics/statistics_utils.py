bfea879af24c2f3e3278d079b21b5c46c8215b65
kamkali <kamkalis@gmail.com>
2020-08-14 17:26:01 +0200

    [BD-89] Refactor and fixes

--- utils/statistics/statistics_utils.py
+++ utils/statistics/statistics_utils.py
@@ -11,13 +11,13 @@ TN = 'true_negatives'
 TP = 'true_positives'
 FAR = 'false_acceptance_rate'
 FRR = 'false_rejection_rate'
-ROUND_DIGITS = 5
+ROUND_DIGITS = 4
 
 
 class StatisticsUtils:
     def __init__(self, ml_model_results: List):
         self.__results: List[History] = ml_model_results
-        self.plotter = PlottingUtils()
+        self.__plotter = PlottingUtils()
 
     def calculate_all_statistics(self):
         calculated_statistics = dict()
@@ -34,7 +34,7 @@ class StatisticsUtils:
         calculated_statistics[f"{LOSS}_plot"] = self.create_model_loss_training_plot()
         calculated_statistics["percentiles_hist"] = self.create_model_accuracy_percentile_histogram()
 
-        self.plotter.save_plotted_data_to_csv()
+        self.__plotter.save_plotted_data_to_csv()
 
         return calculated_statistics
 
@@ -48,16 +48,16 @@ class StatisticsUtils:
         results_matrix = [record.history[metric] for record in self.__results]
         val_results_matrix = [record.history[f'val_{metric}'] for record in self.__results]
 
-        mean_epoch_values = np.mean(results_matrix, axis=0)
-        mean_val_epoch_values = np.mean(val_results_matrix, axis=0)
+        mean_epoch_values = np.mean(results_matrix, axis=0).round(ROUND_DIGITS) * 100   # %
+        mean_val_epoch_values = np.mean(val_results_matrix, axis=0).round(ROUND_DIGITS) * 100   # %
 
-        return self.plotter.create_plot(metric, mean_epoch_values, mean_val_epoch_values)
+        return self.__plotter.create_plot(metric, mean_epoch_values, mean_val_epoch_values)
 
     def create_model_accuracy_percentile_histogram(self):
         acc_results = [record.history[ACC][-1] for record in self.__results]
-        acc_results = np.round(acc_results, ROUND_DIGITS) * 100
+        acc_results = np.round(acc_results, ROUND_DIGITS) * 100     # %
         acc_results.sort()
-        return self.plotter.create_histogram(acc_results)
+        return self.__plotter.create_histogram(acc_results)
 
     def get_mean_accuracy(self):
         return self.__calculate_mean_for_metric(ACC) * 100  # %
@@ -92,12 +92,7 @@ class StatisticsUtils:
     def get_mean_false_acceptance_rate(self):
         return self.__calculate_mean_rate_from_confusion_matrix(FP, TN) * 100  # %
 
-    def __calculate_mean_rate_from_confusion_matrix(self, metric_1, metric_2):
-        confusion_matrix = {}
-        single_rate_list = []
-        for record in self.__results:
-            for metric in [FN, FP, TN, TP]:
-                confusion_matrix[metric] = record.history[metric][-1]
-            single_rate = confusion_matrix[metric_1] / (confusion_matrix[metric_1] + confusion_matrix[metric_2])
-            single_rate_list.append(single_rate)
+    def __calculate_mean_rate_from_confusion_matrix(self, metric1, metric2):
+        single_rate_list = [record.history[metric1][-1] / (record.history[metric1][-1] + record.history[metric2][-1])
+                            for record in self.__results]
         return np.mean(single_rate_list).round(ROUND_DIGITS)
