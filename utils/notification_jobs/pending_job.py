5fb45b156c8ed084f8a1617a5092041b8f77ff6e
kamkali <kamkalis@gmail.com>
2020-08-23 16:14:16 +0200

    Deleting old script and starting fixed model test

--- utils/notification_jobs/pending_job.py
+++ utils/notification_jobs/pending_job.py
@@ -7,14 +7,6 @@ class PendingJobNotification:
     @staticmethod
     def notify():
         start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-<<<<<<< HEAD
-        notifier = SlackNotifier()
-        simple_msg = SimpleMessage.new_builder() \
-            .with_reporter(argv[1]) \
-            .with_commit_hash(f"#{argv[2]}") \
-            .with_job_time(start_time) \
-            .with_header("PENDING JOB") \
-=======
         reporter = argv[1]
         commit_hash = argv[2]
         notifier = SlackNotifier()
@@ -24,7 +16,6 @@ class PendingJobNotification:
             .with_job_time(start_time) \
             .with_header("PENDING JOB") \
             .with_summary(f"Pending job for {reporter}: #{commit_hash}") \
->>>>>>> stat_calc_module
             .build()
         notifier.notify(simple_msg)
 
