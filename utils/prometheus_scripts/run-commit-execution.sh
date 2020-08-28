c230b356c99b1abfcecfa33dce84bf1cae1ca1f9
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-08-28 17:45:38 +0200

    mkdir

--- utils/prometheus_scripts/run-commit-execution.sh
+++ utils/prometheus_scripts/run-commit-execution.sh
@@ -4,6 +4,7 @@ ssh "$PLG_USERNAME"@pro.cyfronet.pl 'cd ~/Bot-Detection && \
 git checkout $OBSERVED_BRANCH && \
 git pull origin $OBSERVED_BRANCH && \
 COMMIT_HASH=$(git rev-parse --short HEAD) && \
-cp -r ~/Bot-Detection/ $RESULTS_PATH/$COMMIT_HASH/Bot-Detection && \
+mkdir $RESULTS_PATH/$COMMIT_HASH/Bot-Detection/ && \
+cp -r ~/Bot-Detection/ $RESULTS_PATH/$COMMIT_HASH/Bot-Detection/ && \
 cd $RESULTS_PATH/$COMMIT_HASH/Bot-Detection/utils/prometheus_scripts && \
 ./run-plgrid-job.sh $RESULTS_PATH/$COMMIT_HASH'
