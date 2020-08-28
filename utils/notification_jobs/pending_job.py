b2f5fb7951ac6b81f2935e53725e490efc7529df
Kalis <50112357+kamkali@users.noreply.github.com>
2020-08-28 18:12:36 +0200

    Adding commit msg to PENDING JOB notification (#22)

--- utils/notification_jobs/pending_job.py
+++ utils/notification_jobs/pending_job.py
@@ -9,12 +9,14 @@ class PendingJobNotification:
         start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         reporter = argv[1]
         commit_hash = argv[2]
+        commit_msg = argv[3]
         notifier = SlackNotifier()
         simple_msg = SimpleMessage.new_builder() \
             .with_reporter(reporter) \
             .with_commit_hash(f"#{commit_hash}") \
             .with_job_time(start_time) \
             .with_header("PENDING JOB") \
+            .with_info_message(f"{commit_msg}") \
             .with_summary(f"Pending job for {reporter}: #{commit_hash}") \
             .build()
         notifier.notify(simple_msg)
