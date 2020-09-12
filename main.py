80047662b740b986b9b29a244cf6c86d85778bed
lothar1998 <33781380+lothar1998@users.noreply.github.com>
2020-09-12 15:48:14 +0200

    final model test

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     try:
 
-        proto_loader = ProtoLoader("/home/piotr/Desktop/dataset2/output")
+        proto_loader = ProtoLoader("/net/people/plglothar/cholda/dataset/output")
         user_dataset = proto_loader.get_list_of_sequences()
         training, validation = get_datasets(user_dataset)
 
