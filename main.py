8d3968960ba6e2aa51c9aee057c6e40cccd2a31a
Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>
2020-09-29 18:10:40 +0200

    Final model (#26)

--- main.py
+++ main.py
@@ -14,95 +14,113 @@ from utils.preproccessing.preprocessor import Preprocessor
 from utils.result_terminator.result_terminator import ResultTerminator
 
 
-NOTIFY = environ.get("NOTIFY")
-CUDA_VISIBLE_DEVICES = environ.get("CUDA_VISIBLE_DEVICES")
+class Job:
+    DEFAULT_PATH = "/net/archive/groups/plggpchdyplo/dataset2/output"
 
-
-if __name__ == '__main__':
-    commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
-    commit_msg = subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode().strip()
-    reporter = subprocess.check_output(['whoami']).decode().strip()
-    start_time = datetime.now()
-
-    if NOTIFY is not None:
-        slack_simple = SimpleMessage()
-        slack_simple_msg = slack_simple.new_builder() \
-            .with_color(Color.BLUE) \
-            .with_reporter(f"{reporter}") \
-            .with_commit_hash(f"#{commit_hash}") \
-            .with_job_time(start_time) \
-            .with_header("RUNNING JOB") \
-            .with_info_message(f"{commit_msg}") \
-            .with_summary(f"Running job: {commit_msg}") \
-            .build()
-        notifier = SlackNotifier()
-        notifier.notify(slack_simple_msg)
-
-    try:
-
-        proto_loader = ProtoLoader("/home/piotr/Desktop/dataset2/output")
+    def __init__(self, dataset_path=DEFAULT_PATH):
+        self.is_notify = environ.get("NOTIFY")
+        self.commit_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode().strip()
+        self.commit_msg = subprocess.check_output(['git', 'log', '-1', '--pretty=%B']).decode().strip()
+        self.reporter = subprocess.check_output(['whoami']).decode().strip()
+        self.start_time = datetime.now()
+        self.notifier = SlackNotifier()
+        proto_loader = ProtoLoader(dataset_path)
         user_dataset = proto_loader.get_list_of_sequences()
         preprocessor = Preprocessor(user_dataset)
-        training, validation = preprocessor.get_datasets()
+        self.training, self.validation = preprocessor.get_datasets()
+        self.statistics = None
 
-        result = []
+    def __notify_start_job(self):
+        if self.is_notify is not None:
+            slack_simple = SimpleMessage()
+            slack_simple_msg = slack_simple.new_builder() \
+                .with_color(Color.BLUE) \
+                .with_reporter(f"{self.reporter}") \
+                .with_commit_hash(f"#{self.commit_hash}") \
+                .with_job_time(self.start_time) \
+                .with_header("RUNNING JOB") \
+                .with_info_message(f"{self.commit_msg}") \
+                .with_summary(f"Running job: {self.commit_msg}") \
+                .build()
+            self.notifier.notify(slack_simple_msg)
 
-        mirrored_strategy = tf.distribute.MirroredStrategy()
-
-        for i in range(10):
-            with mirrored_strategy.scope():
-                model = InceptionV3(training, validation)
-                result.append(model.run())
-
-        end_time = datetime.now()
-        end_time_str = end_time.strftime('%Y-%m-%d %H:%M:%S.%f')
-
-        job_time = end_time - start_time
-
-        statistics = StatisticsUtils(result)
-
-        acc = statistics.get_mean_accuracy()
-        loss = statistics.get_mean_loss()
-        if NOTIFY is not None:
+    def __notify_done_job(self, job_time, acc, loss):
+        if self.is_notify is not None:
             slack_results = ResultMessage()
             slack_result_msg = slack_results.new_builder() \
                 .with_color(Color.GREEN) \
                 .with_job_time(f"{job_time}") \
-                .with_commit_hash(f"#{commit_hash}") \
-                .with_reporter(f"{reporter}") \
+                .with_commit_hash(f"#{self.commit_hash}") \
+                .with_reporter(f"{self.reporter}") \
                 .with_accuracy(f"{acc}%") \
                 .with_loss(f"{loss}%") \
-                .with_accuracy_chart(f"{statistics.create_model_accuracy_training_plot()}") \
-                .with_loss_chart(f"{statistics.create_model_loss_training_plot()}") \
-                .with_percentile_chart(f"{statistics.create_model_accuracy_percentile_histogram()}") \
-                .with_summary(f"Completed job #{commit_hash}") \
+                .with_accuracy_chart(f"{self.statistics.create_model_accuracy_training_plot()}") \
+                .with_loss_chart(f"{self.statistics.create_model_loss_training_plot()}") \
+                .with_percentile_chart(f"{self.statistics.create_model_accuracy_percentile_histogram()}") \
+                .with_summary(f"Completed job #{self.commit_hash}") \
+                .with_false_acceptance_rate(f"{self.statistics.get_mean_false_acceptance_rate()}%") \
+                .with_false_negatives(f"{self.statistics.get_mean_false_negatives()}") \
+                .with_false_positives(f"{self.statistics.get_mean_false_positives()}") \
+                .with_false_rejection_rate(f"{self.statistics.get_mean_false_rejection_rate()}%") \
+                .with_true_negatives(f"{self.statistics.get_mean_true_negatives()}") \
+                .with_true_positives(f"{self.statistics.get_mean_true_positives()}") \
                 .build()
-                # .with_false_acceptance_rate(f"{statistics.get_mean_false_acceptance_rate()}%") \
-                # .with_false_negatives(f"{statistics.get_mean_false_negatives()}") \
-                # .with_false_positives(f"{statistics.get_mean_false_positives()}") \
-                # .with_false_rejection_rate(f"{statistics.get_mean_false_rejection_rate()}%") \
-                # .with_true_negatives(f"{statistics.get_mean_true_negatives()}") \
-                # .with_true_positives(f"{statistics.get_mean_true_positives()}") \
 
-            notifier.notify(slack_result_msg)
+            self.notifier.notify(slack_result_msg)
 
-        results_dict = {"commit_hash": commit_hash,
+    def __notify_crash_job(self, exception):
+        if self.is_notify is not None:
+            crashed_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+            slack_simple = SimpleMessage()
+            slack_err_msg = slack_simple.new_builder() \
+                .with_color(Color.RED) \
+                .with_reporter(f"{self.reporter}") \
+                .with_commit_hash(f"#{self.commit_hash}") \
+                .with_job_time(crashed_time) \
+                .with_header("CRASHED JOB") \
+                .with_info_message(f"Job crashed: {exception}") \
+                .build()
+            self.notifier.notify(slack_err_msg)
+        else:
+            raise exception
+
+    def __compute_statistics(self, result):
+        self.statistics = StatisticsUtils(result)
+        acc = self.statistics.get_mean_accuracy()
+        loss = self.statistics.get_mean_loss()
+        return acc, loss
+
+    def __terminate(self, acc, loss):
+        results_dict = {"commit_hash": self.commit_hash,
                         Metric.ACC.value: acc,
                         Metric.LOSS.value: loss}
 
         ResultTerminator(["commit_hash", Metric.ACC.value, Metric.LOSS.value]).terminate(results_dict)
 
-    except Exception as e:
-        if NOTIFY is not None:
-            crashed_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-            slack_err_msg = slack_simple.new_builder() \
-                .with_color(Color.RED) \
-                .with_reporter(f"{reporter}") \
-                .with_commit_hash(f"#{commit_hash}") \
-                .with_job_time(crashed_time) \
-                .with_header("CRASHED JOB") \
-                .with_info_message(f"Job crashed: {e}") \
-                .build()
-            notifier.notify(slack_err_msg)
-        else:
-            raise e
+    def run(self, model_execution_count):
+        self.__notify_start_job()
+        try:
+            result = []
+
+            mirrored_strategy = tf.distribute.MirroredStrategy()
+
+            for i in range(model_execution_count):
+                with mirrored_strategy.scope():
+                    model = InceptionV3(self.training, self.validation)
+                    result.append(model.run())
+
+            job_time = self.__compute_job_time()
+            acc, loss = self.__compute_statistics(result)
+            self.__notify_done_job(job_time, acc, loss)
+            self.__terminate(acc, loss)
+
+        except Exception as e:
+            self.__notify_crash_job(e)
+
+    def __compute_job_time(self):
+        return datetime.now() - self.start_time
+
+
+if __name__ == '__main__':
+    job = Job()
+    job.run(10)
